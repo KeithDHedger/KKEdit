d8e54e2f3300098e31a9a08d2fbece5d14cc8cec
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-03-28 20:58:51 +0000

    done basic menu
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@2 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/main.cpp
+++ KKEdit/src/main.cpp
@@ -2,14 +2,14 @@
  *
  * K.D.Hedger 2012 <kdhedger68713@gmail.com>
  *
- */
-
+*/
+ 
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <string.h>
 #include <sys/stat.h>
 
-GtkWidget *window = NULL;
+#include "globals.h"
 
 void shutdown(GtkWidget* widget,gpointer data)
 {
@@ -18,9 +18,91 @@ void shutdown(GtkWidget* widget,gpointer data)
 
 int main(int argc,char **argv)
 {
+	GtkWidget*		vbox;
+	GtkWidget*		menuitem;
+	GtkWidget*		menu;
+
 	gtk_init(&argc,&argv);
 	window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(G_OBJECT(window),"delete-event",G_CALLBACK(shutdown),NULL);
+
+	notebook=(GtkNotebook*)gtk_notebook_new();
+	vbox=gtk_vbox_new(FALSE,0);
+	menubar=gtk_menu_bar_new();
+
+	gtk_box_pack_start(GTK_BOX(vbox),(GtkWidget*)menubar,false,true,0);
+	menunav=gtk_menu_item_new_with_label("Navigation");
+	menu=gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menunav),menu);
+
+
+//file menu
+	menufile=gtk_menu_item_new_with_label("File");
+	menu=gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menufile),menu);
+
+	menuitem=gtk_menu_item_new_with_label("New");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Open");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Save");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Save As");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Close");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	menuitem=gtk_menu_item_new_with_label("Exit");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	gtk_signal_connect(GTK_OBJECT(menuitem),"activate",G_CALLBACK(shutdown),NULL);
+
+//edit menu
+	menuedit=gtk_menu_item_new_with_label("Edit");
+	menu=gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menuedit),menu);
+	menuitem=gtk_menu_item_new_with_label("Undo");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Redo");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	menuitem=gtk_menu_item_new_with_label("Cut");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Copy");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Paste");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	menuitem=gtk_menu_item_new_with_label("Find");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_menu_item_new_with_label("Replace");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+//bookmarks
+	menubookmark=gtk_menu_item_new_with_label("Bookmarks");
+	menu=gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menubookmark),menu);
+	menuitem=gtk_menu_item_new_with_label("Add Bookmark");
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menufile);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menuedit);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menunav);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menubookmark);
+
+	gtk_container_add(GTK_CONTAINER(window),(GtkWidget*)vbox);
+	
 	gtk_widget_show_all(window);
 	gtk_main();
 }
