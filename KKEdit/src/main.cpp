98b8737cdb6cc66dce716d53279d9d289b2fac9f
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-03-29 13:05:50 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@3 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/main.cpp
+++ KKEdit/src/main.cpp
@@ -9,6 +9,11 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#include <gtksourceview/gtksourceview.h>
+#include <gtksourceview/gtksourcebuffer.h>
+#include <gtksourceview/gtksourcelanguage.h>
+#include <gtksourceview/gtksourcelanguagemanager.h>
+
 #include "globals.h"
 
 void shutdown(GtkWidget* widget,gpointer data)
@@ -21,7 +26,12 @@ int main(int argc,char **argv)
 	GtkWidget*		vbox;
 	GtkWidget*		menuitem;
 	GtkWidget*		menu;
-
+	GtkWidget*		scrolled_win;
+	GtkWidget*		sourceview;
+	GtkSourceLanguageManager *lm;
+	GtkSourceBuffer *buffer;
+	PangoFontDescription *font_desc;
+	
 	gtk_init(&argc,&argv);
 	window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(G_OBJECT(window),"delete-event",G_CALLBACK(shutdown),NULL);
@@ -31,34 +41,56 @@ int main(int argc,char **argv)
 	menubar=gtk_menu_bar_new();
 
 	gtk_box_pack_start(GTK_BOX(vbox),(GtkWidget*)menubar,false,true,0);
+	gtk_box_pack_start(GTK_BOX(vbox),(GtkWidget*)notebook,true,true,0);
+
+	scrolled_win=gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_win),GTK_POLICY_ALWAYS, GTK_POLICY_ALWAYS);
+	lm=gtk_source_language_manager_new();
+   
+	buffer=GTK_SOURCE_BUFFER(gtk_source_buffer_new(NULL));
+	g_object_ref(lm);
+	g_object_set_data_full(G_OBJECT(buffer),"languages-manager",lm,(GDestroyNotify) g_object_unref);
+
+	sourceview=gtk_source_view_new_with_buffer(buffer);
+    
+	font_desc = pango_font_description_from_string("mono 8");
+	gtk_widget_modify_font(sourceview, font_desc);
+	pango_font_description_free(font_desc);
+
+	gtk_container_add(GTK_CONTAINER(scrolled_win),GTK_WIDGET(sourceview));
+	gtk_container_add(GTK_CONTAINER(notebook),scrolled_win);
+
+
 	menunav=gtk_menu_item_new_with_label("Navigation");
 	menu=gtk_menu_new();
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menunav),menu);
 
-
+//accel_group = gtk_accel_group_new();
+//  gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+  
 //file menu
 	menufile=gtk_menu_item_new_with_label("File");
 	menu=gtk_menu_new();
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menufile),menu);
 
-	menuitem=gtk_menu_item_new_with_label("New");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Open");
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_separator_menu_item_new();
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Save");
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Save As");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_OPEN,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 	menuitem=gtk_separator_menu_item_new();
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Close");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE,NULL);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_SAVE_AS,NULL);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_CLOSE,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 	menuitem=gtk_separator_menu_item_new();
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 
-	menuitem=gtk_menu_item_new_with_label("Exit");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 	gtk_signal_connect(GTK_OBJECT(menuitem),"activate",G_CALLBACK(shutdown),NULL);
 
@@ -66,25 +98,25 @@ int main(int argc,char **argv)
 	menuedit=gtk_menu_item_new_with_label("Edit");
 	menu=gtk_menu_new();
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menuedit),menu);
-	menuitem=gtk_menu_item_new_with_label("Undo");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_UNDO,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Redo");
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_separator_menu_item_new();
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-
-	menuitem=gtk_menu_item_new_with_label("Cut");
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Copy");
-	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Paste");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_REDO,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 	menuitem=gtk_separator_menu_item_new();
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 
-	menuitem=gtk_menu_item_new_with_label("Find");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_CUT,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
-	menuitem=gtk_menu_item_new_with_label("Replace");
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_COPY,NULL);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_PASTE,NULL);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_separator_menu_item_new();
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_FIND,NULL);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_FIND_AND_REPLACE,NULL);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
 
 //bookmarks
@@ -100,6 +132,10 @@ int main(int argc,char **argv)
 	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menuedit);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menunav);
 	gtk_menu_shell_append(GTK_MENU_SHELL(menubar),menubookmark);
+
+//GtkStockItem* xx;
+//gtk_stock_lookup(GTK_STOCK_NEW,xx);
+//printf("\n%s\n",xx);
 
 	gtk_container_add(GTK_CONTAINER(window),(GtkWidget*)vbox);
 	
