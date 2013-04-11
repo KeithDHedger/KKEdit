3abe7d57ea21dbfae04c9e0bc458c00d25ebf2c9
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-04-11 18:43:28 +0000

    doing case insens search
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@113 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/callbacks.cpp
+++ KKEdit/src/callbacks.cpp
@@ -7,6 +7,7 @@
 */
 
 #include <gtk/gtk.h>
+#include <gtksourceview/gtksourceiter.h>
 
 #include "globals.h"
 #include "files.h"
@@ -267,6 +268,11 @@ void doFindReplace(GtkDialog *dialog,gint response_id,gpointer user_data)
 {
 	pageStruct* page=getPageStructPtr(-1);
 	gchar*		selectedtext=NULL;
+GtkTextIter start;
+GtkTextIter end;
+gchar* ttext;
+char* ptr=NULL;
+long s,e;
 
 	gtk_text_buffer_begin_user_action((GtkTextBuffer*)page->buffer);
 
@@ -274,9 +280,30 @@ void doFindReplace(GtkDialog *dialog,gint response_id,gpointer user_data)
 		{
 //forward search
 			case FINDNEXT:
+/*			
+			gtk_text_buffer_get_start_iter((GtkTextBuffer *)page->buffer,&start);
+			gtk_text_buffer_get_end_iter((GtkTextBuffer *)page->buffer,&end);
+			
+				ttext=gtk_text_buffer_get_text((GtkTextBuffer *)page->buffer,&start,&end,true);
+				//printf("%s\n",(char*)ttext);
+				ptr=strstr(ttext,gtk_entry_get_text((GtkEntry*)findBox));
+				if (ptr!=NULL)
+					{
+						s=(long)ttext;
+						e=(long)ptr;
+						gtk_text_buffer_get_iter_at_offset((GtkTextBuffer *)page->buffer,&start,e-s);
+						gtk_text_buffer_get_iter_at_offset((GtkTextBuffer *)page->buffer,&end,e-s+5);
+						gtk_text_buffer_select_range((GtkTextBuffer*)page->buffer,&start,&end);
+					}
+					break;
+*/			
 				if(!gtk_text_buffer_get_selection_bounds((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end))
 					gtk_text_buffer_get_iter_at_mark((GtkTextBuffer*)page->buffer,&page->iter,gtk_text_buffer_get_insert((GtkTextBuffer*)page->buffer));
-				if(gtk_text_iter_forward_search(&page->match_end,gtk_entry_get_text((GtkEntry*)findBox),GTK_TEXT_SEARCH_TEXT_ONLY,&page->match_start,&page->match_end,NULL))
+				
+				
+				//if(gtk_text_iter_forward_search(&page->match_end,gtk_entry_get_text((GtkEntry*)findBox),GTK_TEXT_SEARCH_TEXT_ONLY,&page->match_start,&page->match_end,NULL))
+				if(gtk_source_iter_forward_search(&page->match_end,gtk_entry_get_text((GtkEntry*)findBox),
+                                                         (GtkSourceSearchFlags)(GTK_SOURCE_SEARCH_TEXT_ONLY|GTK_SOURCE_SEARCH_CASE_INSENSITIVE),&page->match_start,&page->match_end,NULL))
 					{
 						gtk_text_buffer_select_range((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end);
 						gtk_text_view_scroll_to_iter((GtkTextView*)page->view,&page->match_start,0,true,0,0.5);
@@ -287,7 +314,7 @@ void doFindReplace(GtkDialog *dialog,gint response_id,gpointer user_data)
 			case FINDPREV:
 				if(!gtk_text_buffer_get_selection_bounds((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end))
 					gtk_text_buffer_get_iter_at_mark((GtkTextBuffer*)page->buffer,&page->iter,gtk_text_buffer_get_insert((GtkTextBuffer*)page->buffer));
-				if(gtk_text_iter_backward_search(&page->match_start,gtk_entry_get_text((GtkEntry*)findBox),GTK_TEXT_SEARCH_TEXT_ONLY,&page->match_start,&page->match_end,NULL))
+				if(gtk_source_iter_backward_search(&page->match_start,gtk_entry_get_text((GtkEntry*)findBox),(GtkSourceSearchFlags)(GTK_SOURCE_SEARCH_TEXT_ONLY|GTK_SOURCE_SEARCH_CASE_INSENSITIVE),&page->match_start,&page->match_end,NULL))
 					{
 						gtk_text_buffer_select_range((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end);
 						gtk_text_view_scroll_to_iter((GtkTextView*)page->view,&page->match_start,0,true,0,0.5);
@@ -304,7 +331,7 @@ void doFindReplace(GtkDialog *dialog,gint response_id,gpointer user_data)
 								gtk_text_buffer_delete((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end);
 								gtk_text_buffer_insert((GtkTextBuffer*)page->buffer,&page->match_start,gtk_entry_get_text((GtkEntry*)replaceBox),-1);
 								gtk_text_buffer_get_iter_at_mark((GtkTextBuffer*)page->buffer,&page->iter,gtk_text_buffer_get_insert((GtkTextBuffer*)page->buffer));
-								if(gtk_text_iter_forward_search(&page->iter,gtk_entry_get_text((GtkEntry*)findBox),GTK_TEXT_SEARCH_TEXT_ONLY,&page->match_start,&page->match_end,NULL))
+								if(gtk_source_iter_forward_search(&page->iter,gtk_entry_get_text((GtkEntry*)findBox),(GtkSourceSearchFlags)(GTK_SOURCE_SEARCH_TEXT_ONLY|GTK_SOURCE_SEARCH_CASE_INSENSITIVE),&page->match_start,&page->match_end,NULL))
 									{
 										gtk_text_buffer_select_range((GtkTextBuffer*)page->buffer,&page->match_start,&page->match_end);
 										gtk_text_view_scroll_to_iter((GtkTextView*)page->view,&page->match_start,0,true,0,0.5);
