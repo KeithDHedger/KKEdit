c02fb913b0e4f244de561c2182fd6b0b55de089e
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-04-22 11:44:06 +0000

    made single instance
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@210 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/globals.h
+++ KKEdit/src/globals.h
@@ -32,9 +32,6 @@
 #define VALIDFILENAMECHARS "[A-Za-z0-9_-./]"
 #define VALIDFUNCTIONCHARS "[A-Za-z0-9_*]"
 
-//#define COMMAND_ACTIVATE 1
-//#define COMMAND_OPENFILE 2
-
 struct pageStruct
 {
 	GtkScrolledWindow*	pageWindow;
