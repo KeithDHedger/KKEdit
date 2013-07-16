dca29eeebfa2688f8eb19b5142955ce9900710aa
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-07-16 20:10:12 +0000

    started adding spell check
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@344 923dfa19-3233-4c9e-86b8-84ae44b222e5

---
+++ KKEdit/src/spellcheck.cpp
@@ -0,0 +1,42 @@
+/******************************************************
+*
+*     ©keithhedger Tue 16 Jul 20:46:19 BST 2013
+*     kdhedger68713@gmail.com
+*
+*     spellcheck.cpp
+* 
+******************************************************/
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <gtksourceview/gtksourcebuffer.h>
+#include <gtksourceview/gtksourceview.h>
+
+#include "config.h"
+#include "globals.h"
+
+void checkWord(GtkWidget* widget,gpointer data)
+{
+	pageStruct*	page=getPageStructPtr(-1);
+	GtkTextIter	start;
+	GtkTextIter	end;
+	char*		selection=NULL;
+	char*		command;
+
+	if(gtk_text_buffer_get_selection_bounds((GtkTextBuffer*)page->buffer,&start,&end))
+		{
+			selection=gtk_text_buffer_get_text((GtkTextBuffer*)page->buffer,&start,&end,false);
+			if(selection==NULL)
+				return;
+		}
+	else
+		return;
+
+	printf("word=%s\n",selection);
+	asprintf(&command,"echo -n \"%s\"|ispell -a|grep \":\"",selection);
+	system(command);
+	g_free(selection);
+
+}
---
+++ KKEdit/src/spellcheck.h
@@ -0,0 +1,16 @@
+/******************************************************
+*
+*     ©keithhedger Tue 16 Jul 20:46:19 BST 2013
+*     kdhedger68713@gmail.com
+*
+*     spellcheck.h
+* 
+******************************************************/
+#include <gtk/gtk.h>
+
+#ifndef _SPELLCHECK_
+#define _SPELLCHECK_
+
+void checkWord(GtkWidget* widget,gpointer data);
+
+#endif
