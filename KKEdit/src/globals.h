36ffc1974bedce98ae302359e03f6f56eb784bb5
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2013-09-06 12:31:52 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@398 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/globals.h
+++ KKEdit/src/globals.h
@@ -30,7 +30,7 @@
 
 #define MYEMAIL "kdhedger68713@gmail.com"
 #define MYWEBSITE "http://keithhedger.hostingsiteforfree.com"
-#define KKEDIT_VERSION "0.0.21"
+#define KKEDIT_VERSION "0.0.22"
 
 #define TOOL_ASYNC 8
 #define TOOL_IGNORE_OP 0
