43d0e6342ba510e2d48be35700b57fed3188d7fc
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2014-07-01 14:50:56 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@823 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/main.cpp
+++ KKEdit/src/main.cpp
@@ -152,19 +152,6 @@ void readConfig(void)
 	g_free(filename);
 }
 
-//void gen_random(char *s, const int len) {
-//srand(time(0));
-//    static const char alphanum[] =
-//        "0123456789"
-//        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//        "abcdefghijklmnopqrstuvwxyz";
-//
-//    for (int i = 0; i < len; ++i) {
-//        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
-//    }
-//
-//    s[len] = 0;
-//}
 void init(void)
 {
 	char*	filename;
