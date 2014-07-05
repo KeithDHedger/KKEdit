f2ec1a7206e670b67691314ffdea0bf1aadfdf6e
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2014-07-05 18:33:51 +0000

    started plugins
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@835 923dfa19-3233-4c9e-86b8-84ae44b222e5

---
+++ KKEdit/resources/plugs/kkedit-test-plug.c
@@ -0,0 +1,50 @@
+/*
+  plugins/libplug_01.c
+  plugin example with GLib: plugin
+  Compile:
+  gcc -fPIC -Wall -c libplug_01.c `pkg-config --cflags --libs gtk+-2.0 gmodule-2.0` -o libplug_01.o
+  gcc -shared -Wl,-soname,libplug_01.so -o libplug_01.so libplug_01.o -lc
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <gtk/gtk.h>
+#include <gmodule.h>
+
+int module_variable = 42;
+
+G_MODULE_EXPORT extern gchar module_text[] = "This is module_text";
+
+G_MODULE_EXPORT extern gint module_test (gchar * szoveg)  {
+    GtkWidget * plugin_window = NULL;
+    g_message ("this is module text: %s\n", szoveg);
+    g_message ("this is module variable: %d\n", module_variable);
+    plugin_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW(plugin_window), "plugin_window");
+    gtk_widget_show (plugin_window);
+    return module_variable;
+}
+
+const gchar* g_module_check_init(GModule *module)
+{
+	perror("doin ininit");
+	return(NULL);
+}
+
+const gchar* g_module_unload(GModule *module)
+{
+	perror("doin cleanup");
+	return(NULL);
+}
+
+int addMenus (gpointer menulist)
+{
+	printf("adding  plug menus\n");
+	return(0);
+}
+
+
+
+
+
