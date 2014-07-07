c0018ce9a252e75614bdf4f7c98d6387dc097298
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2014-07-07 19:51:03 +0000

    code clean
    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@850 923dfa19-3233-4c9e-86b8-84ae44b222e5

--- KKEdit/src/guis.cpp
+++ KKEdit/src/guis.cpp
@@ -1286,8 +1286,99 @@ void addRecentToMenu(GtkRecentChooser* chooser,GtkWidget* menu)
 		}
 }
 
+enum {COLUMN_ENABLE,COLUMN_PLUGIN,NUM_COLUMNS};
+
+void enableToggled(GtkCellRendererToggle *cell,gchar *path_str,gpointer data)
+{
+	GtkTreeModel *model = (GtkTreeModel *)data;
+	GtkTreeIter  iter;
+	GtkTreePath *path = gtk_tree_path_new_from_string (path_str);
+	gboolean fixed;
+
+	/* get toggled iter */
+	gtk_tree_model_get_iter (model, &iter, path);
+	gtk_tree_model_get (model, &iter, COLUMN_ENABLE, &fixed, -1);
+
+	/* do something with the value */
+	fixed ^= 1;
+
+	/* set new value */
+	gtk_list_store_set (GTK_LIST_STORE (model), &iter, COLUMN_ENABLE, fixed, -1);
+
+	/* clean up */
+	gtk_tree_path_free (path);
+}
+
 void doPlugPrefs(void)
 {
+
+	GtkWidget*		vbox;
+	GtkWidget*		menuitem;
+	GtkWidget*		menu;
+	GtkWidget*		image;
+	GtkWidget*		menurecent;
+	GtkWidget*		scrollbox;
+	GtkWidget*		mainwindowbox;
+	GtkWidget*		plugwindow;
+	GtkListStore*	store;
+	GtkTreeIter		iter;
+	GtkTreeModel*	model=NULL;
+	GtkWidget*		treeview;
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GtkWidget*		button;
+	GtkWidget*		hbox;
+
+	plugwindow=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	vbox=gtk_vbox_new(false,0);
+//	scrollbox=gtk_scrolled_window_new(NULL,NULL);
+//	gtk_box_pack_start(GTK_BOX(scrollbox),(GtkWidget*)toolBar,true,true,0);
+	store=gtk_list_store_new (NUM_COLUMNS,G_TYPE_BOOLEAN,G_TYPE_STRING);
+
+	gtk_list_store_append(store,&iter);
+	gtk_list_store_set(store,&iter,COLUMN_ENABLE,true,COLUMN_PLUGIN,"test1",-1);
+	gtk_list_store_append(store,&iter);
+	gtk_list_store_set(store,&iter,COLUMN_ENABLE,false,COLUMN_PLUGIN,"test2",-1);
+
+	model=GTK_TREE_MODEL(store);
+	treeview=gtk_tree_view_new_with_model(model);
+	g_object_unref (model);
+	gtk_container_add (GTK_CONTAINER (vbox),treeview);
+
+//enable
+	renderer=gtk_cell_renderer_toggle_new();
+	g_signal_connect(renderer,"toggled",G_CALLBACK(enableToggled),model);
+
+	column=gtk_tree_view_column_new_with_attributes("Enable",renderer,"active",COLUMN_ENABLE,NULL);
+	gtk_tree_view_column_set_sizing(GTK_TREE_VIEW_COLUMN(column),GTK_TREE_VIEW_COLUMN_FIXED);
+	gtk_tree_view_column_set_fixed_width(GTK_TREE_VIEW_COLUMN(column),50);
+	gtk_tree_view_append_column((GtkTreeView*)treeview,column);
+
+//plug
+	renderer=gtk_cell_renderer_text_new();
+	column=gtk_tree_view_column_new_with_attributes("Plug In",renderer,"text",COLUMN_PLUGIN,NULL);
+	gtk_tree_view_column_set_sort_column_id(column,COLUMN_PLUGIN);
+	gtk_tree_view_append_column((GtkTreeView*)treeview,column);
+
+
+	hbox=gtk_hbox_new(true,4);
+	gtk_container_add (GTK_CONTAINER(vbox),hbox);
+//plugin prefs
+	button=gtk_button_new_from_stock(GTK_STOCK_PREFERENCES);
+	gtk_box_pack_start((GtkBox*)hbox,button,true,true,4);
+//plugin about
+	button=gtk_button_new_from_stock(GTK_STOCK_ABOUT);
+	gtk_box_pack_start((GtkBox*)hbox,button,true,true,4);
+//close
+	button=gtk_button_new_from_stock(GTK_STOCK_APPLY);
+	gtk_box_pack_start((GtkBox*)hbox,button,true,true,4);
+//apply
+	button=gtk_button_new_from_stock(GTK_STOCK_CANCEL);
+	gtk_box_pack_start((GtkBox*)hbox,button,true,true,4);
+
+	gtk_container_add (GTK_CONTAINER(plugwindow),vbox);
+	gtk_widget_show_all(plugwindow);
 }
 
 void buildMainGui(void)
