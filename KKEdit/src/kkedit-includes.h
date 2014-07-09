e4e9833f7bd75f23a1fb6d2ab9864b1826b5298b
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2014-07-09 16:59:43 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@870 923dfa19-3233-4c9e-86b8-84ae44b222e5

---
+++ KKEdit/src/kkedit-includes.h
@@ -0,0 +1,63 @@
+/*
+ *
+ * K.D.Hedger 2014 <kdhedger68713@gmail.com>
+ *
+ * kkedit-includes.h
+ *
+*/
+
+#ifndef _KKEDIT_INCLUDES_
+#define _KKEDIT_INCLUDES_
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <glib.h>
+
+#include <gtksourceview/gtksourcestyleschememanager.h>
+#include <gtksourceview/gtksourceview.h>
+#include <gtksourceview/gtksourcebuffer.h>
+#include <gtksourceview/gtksourcelanguage.h>
+#include <gtksourceview/gtksourceiter.h>
+
+#include <gtksourceview/gtksourcelanguagemanager.h>
+#include <gtksourceview/gtksourceprintcompositor.h>
+#include <ctype.h>
+#include <gdk/gdkkeysyms.h>
+
+#include <libgen.h>
+#include <time.h>
+
+
+#include <unique/unique.h>
+
+
+#include "config.h"
+#include "sliceclass.h"
+#include "kkedit-plugins.h"
+#include "textbuffer.h"
+#include "backclass.h"
+//#include "pluginclass.h"
+
+#ifdef _ASPELL_
+#include <aspell.h>
+#endif
+
+#ifdef _BUILDDOCVIEWER_
+#include <webkit/webkit.h>
+#endif
+
+#include "globals.h"
+
+#include "callbacks.h"
+#include "guis.h"
+#include "searchcallbacks.h"
+#include "navcallbacks.h"
+#include "spellcheck.h"
+#include "files.h"
+#include "encoding.h"
+#endif
+
+
+
