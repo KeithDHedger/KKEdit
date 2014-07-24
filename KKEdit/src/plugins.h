80b33fd8f53ba75d0d9e9a1e3a98771308c4a837
keithhedger <keithhedger@923dfa19-3233-4c9e-86b8-84ae44b222e5>
2014-07-24 15:55:31 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEdit@906 923dfa19-3233-4c9e-86b8-84ae44b222e5

---
+++ KKEdit/src/plugins.h
@@ -0,0 +1,17 @@
+/******************************************************
+*
+*     ©keithhedger Wed 23 Jul 15:34:30 BST 2014
+*     kdhedger68713@gmail.com
+*
+*     plugins.h
+* 
+******************************************************/
+
+#ifndef _PLUGINSTUFF_
+#define _PLUGINSTUFF_
+
+void doPlugPrefs(void);
+void showSide(bool left);
+void hideSide(bool left);
+
+#endif
