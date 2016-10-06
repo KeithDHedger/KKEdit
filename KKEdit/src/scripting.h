b301cc7bffe84a098c4d5b2cf3407588c797584f
K D Hedger <kdhedger68713@gmail.com>
2016-10-06 12:13:35 +0100

    added scripting

---
+++ KKEdit/src/scripting.h
@@ -0,0 +1,25 @@
+/*
+ *
+ * ©K. D. Hedger. Thu  6 Oct 12:10:04 BST 2016 kdhedger68713@gmail.com
+
+ * This file (scripting.h) is part of KKEdit.
+
+ * KKEdit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * KKEdit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with KKEdit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SCRIPTING_
+#define _SCRIPTING_
+
+
+#endif
