90f8886000bf8b1dbd0caad3e9f5ab05845590a4
K D Hedger <kdhedger68713@gmail.com>
2020-10-15 12:16:05 +0100

    Fixed french help page

--- KKEdit/src/callbacks.cpp
+++ KKEdit/src/callbacks.cpp
@@ -800,16 +800,12 @@ VISIBLE void openHelp(GtkWidget *widget,gpointer data)
 	if((long)data==0)
 		sinkReturn=asprintf(&thePage,"file://%s/help.%s.html",GHELPFOLDER,lang);
 	else
-//		sinkReturn=asprintf(&thePage,"http://kkedit.freeddns.org/");
-		//sinkReturn=asprintf(&thePage,"http://keithdhedger.github.io/KKEdit/");
 		sinkReturn=asprintf(&thePage,"http://keithdhedger.github.io/KKEdit/help.%s.html",lang);
 	showDocView(USEURI,(char*)"KKEdit",DOCVIEW_KKEDIT_HELP_LABEL);
 #else
 	if((long)data==0)
 		sinkReturn=asprintf(&thePage,"%s %s/help.%s.html",browserCommand,GHELPFOLDER,lang);
 	else
-//		sinkReturn=asprintf(&thePage,"%s http://kkedit.freeddns.org/",browserCommand);
-//		sinkReturn=asprintf(&thePage,"%s http://keithdhedger.github.io/KKEdit/",browserCommand);
 		sinkReturn=asprintf(&thePage,"%s http://keithdhedger.github.io/KKEdit/help.%s.html",browserCommand,lang);
 
 	runCommand(thePage,NULL,false,8,0,(char*)DOCVIEW_KKEDIT_HELP_LABEL);
